7012df996f013f3c959a496ba231e51d3b0e69b6
Alexandre Fayolle <afayolle.ml@free.fr>
2012-08-27 15:57:19 +0200

    Add a TransceiverESME class
    the code uses gevent to handle the multiplexing

--- smpp/esme.py
+++ smpp/esme.py
@@ -1,3 +1,6 @@
+from gevent import monkey, Greenlet
+from gevent.event import AsyncResult
+monkey.patch_all()
 import socket
 
 from pdu_builder import *
@@ -129,3 +132,160 @@ class ESME(object):
             raise StateError('cannot submit multi sm in state %s', self.state)
 
 
+class TransceiverESME(ESME):
+    def __init__(self):
+        super(TransceiverESME, self).__init__()
+        self.pending_response = {} # sequence_number -> event
+        self._greenlet = Greenlet(self._receive)
+
+    def _receive(self):
+        while True:
+            pdu = self._recv()
+            if pdu is not None:
+                self.logger.info('received %s', pdu)
+                self._handleInPDU(pdu)
+            else:
+                self.disconnect()
+
+    def _handleInPDU(self, pdu):
+        if pdu.sequence_number in self.pending_response:
+            self.logger.debug('received response for %d', pdu.sequence_number)
+            event = self.pending_response.pop(pdu.sequence_number)
+            event.set(pdu)
+        else:
+            handler = getattr(self, '_handle_%s' % pdu.command_id, None)
+            if handler is not None:
+                handler(pdu)
+            else:
+                self.logger.critical('No handler for pdu %s', pdu.command_id)
+
+    def _handle_enquire_link(self, pdu, **kwargs):
+        pdu = EnquireLinkResp(pdu.sequence_number, **dict(self.defaults, **kwargs))
+        self.logger.debug('enquire_link_resp %s', pdu)
+        self.conn.send(pdu.get_bin())
+
+    def _handle_unbind(self, pdu, **kwargs):
+        pdu = UnbindResp(pdu.sequence_number, **dict(self.defaults, **kwargs))
+        self.logger.info('unbind_resp %s', pdu)
+        self.conn.send(pdu.get_bin())
+        self.state = 'OPEN'
+        self.disconnect()
+
+    def _handle_deliver_sm(self, pdu, **kwargs):
+        self.logger.info('deliver_sm %s', pdu)
+        try:
+            self.on_receive_sm(pdu)
+        except:
+            self.logger.exception('problem during sm processing')
+            self.logger.warning('not confirming sm processing')
+        else:
+            pdu = DeliverSMResp(pdu.sequence_number, **dict(self.defaults, **kwargs))
+            self.logger.info('deliver_sm_resp %s', pdu)
+
+    def on_receive_sm(self, pdu):
+        short_message = pdu['body']['mandatory_parameters']['short_message']
+        self.logger.info('short message: %s', short_message)
+
+    def _handle_data_sm(self, pdu):
+        self.logger.info('data_sm %s', pdu.obj)
+        raise NotImplementedError()
+
+    def _handle_alert_notification(self, pdu):
+        pass
+
+    def asyncRes(self, sequence_number):
+        defered = AsyncResult()
+        self.pending_response[sequence_number] = defered
+        return defered.get()
+
+    def connect(self):
+        if self.state in ['CLOSED']:
+            super(TransceiverESME, self).connect()
+            self._greenlet.start()
+
+    def disconnect(self):
+        if self.state in ['BOUND_TX', 'BOUND_RX', 'BOUND_TRX']:
+            super(TransceiverESME, self), disconnect()
+        if self.state in ['OPEN']:
+            super(TransceiverESME, self), disconnect()
+            self._greenlet.kill()
+
+    def _unbind(self):
+        if self.state in ['BOUND_TX', 'BOUND_RX', 'BOUND_TRX']:
+            sequence_number = self.sequence_number
+            self.sequence_number +=1
+            pdu = Unbind(sequence_number)
+            self.conn.send(pdu.get_bin())
+            if self._is_ok(self.asyncRes(sequence_number), 'unbind_resp'):
+                self.state = 'OPEN'
+
+    def submit_sm(self, **kwargs):
+        if self.state in ['BOUND_TX', 'BOUND_TRX']:
+            sequence_number = self.sequence_number
+            self.sequence_number +=1
+            pdu = SubmitSM(sequence_number, **dict(self.defaults, **kwargs))
+            self.logger.info('submit_sm %s', pdu)
+            self.conn.send(pdu.get_bin())
+            submit_sm_resp = self.asyncRes(sequence_number)
+            #print self._is_ok(submit_sm_resp, 'submit_sm_resp')
+        else:
+            raise StateError('cannot submit sm in state %s', self.state)
+
+
+    def submit_multi(self, dest_address=[], **kwargs):
+        if self.state in ['BOUND_TX', 'BOUND_TRX']:
+            sequence_number = self.sequence_number
+            self.sequence_number +=1
+            pdu = SubmitMulti(sequence_number, **dict(self.defaults, **kwargs))
+            for item in dest_address:
+                if isinstance(item, str): # assume strings are addresses not lists
+                    pdu.addDestinationAddress(
+                            item,
+                            dest_addr_ton = self.defaults['dest_addr_ton'],
+                            dest_addr_npi = self.defaults['dest_addr_npi'],
+                            )
+                elif isinstance(item, dict):
+                    if item.get('dest_flag') == 1:
+                        pdu.addDestinationAddress(
+                                item.get('destination_addr', ''),
+                                dest_addr_ton = item.get('dest_addr_ton',
+                                    self.defaults['dest_addr_ton']),
+                                dest_addr_npi = item.get('dest_addr_npi',
+                                    self.defaults['dest_addr_npi']),
+                                )
+                    elif item.get('dest_flag') == 2:
+                        pdu.addDistributionList(item.get('dl_name'))
+            self.conn.send(pdu.get_bin())
+            submit_multi_resp = self.asyncRes(sequence_number)
+        else:
+            raise StateError('cannot submit multi sm in state %s', self.state)
+
+    def bind_transceiver(self):
+        if self.state in ['CLOSED']:
+            self.connect()
+        if self.state in ['OPEN']:
+            self.logger.info('bind transceiver')
+            sequence_number = self.sequence_number
+            self.sequence_number +=1
+            pdu = BindTransceiver(sequence_number, **self.defaults)
+            self.conn.send(pdu.get_bin())
+            self.logger.debug('bind_transceiver: waiting for response')
+            response = self.asyncRes(sequence_number)
+            self.logger.debug('bind_transceiver: received response %s', response)
+            if self._is_ok(response,
+                            'bind_transceiver_resp'):
+                self.state = 'BOUND_TRX'
+            else:
+                raise StateError('unexpected response')
+
+    def enquire_link(self, **kwargs):
+        if self.state in ('BOUND_TX', 'BOUND_TRX', 'BOUND_RX'):
+            sequence_number = self.sequence_number
+            self.sequence_number +=1
+            pdu = EnquireLink(sequence_number, **dict(self.defaults, **kwargs))
+            self.logger.info('enquire_link_resp %s', pdu)
+            self.conn.send(pdu.get_bin())
+            if not self._is_ok(self.asyncRes(sequence_number), 'enquire_link_resp'):
+                self.disconnect()
+        else:
+            raise StateError('unbound')
